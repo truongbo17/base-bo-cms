1af4f7409852689295abcdc7390e6097703a33cd
Nguyen Quang Truong <truongnq017@gmail.com>
2024-03-30 16:59:02 +0700

    test

---
+++ resources/views/base/inc/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="app-footer d-print-none">
+    <div class="text-muted ml-auto mr-auto">
+        <p>Chúng tôi luôn cố gắng đưa ra những sản phẩm chất lượng và hoàn thiện nhất tới tay khách hàng. Khách hàng có
+            bất kỳ vấn đề cần hỗ trợ, vui lòng liên hệ: </p><a target="_blank" rel="noopener" href="https://t.me/bo_dev">
+            https://t.me/bo_dev</a>. Trân trọng!
+    </div>
+</footer>
