af9c7f31028769b0805738fc452b4bceced0f3a1
Nguyen Quang Truong <91400591+truongbo17@users.noreply.github.com>
2024-12-25 11:45:24 +0700

    Update footer.blade.php

--- resources/views/base/inc/footer.blade.php
+++ resources/views/base/inc/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="text-muted ml-auto mr-auto text-center mt-2">
     <span class="text-dark" style="font-size: 1rem;">Chúng tôi luôn cố gắng đưa ra những sản phẩm chất lượng và hoàn thiện nhất tới tay khách hàng. Khách hàng có
         bất kỳ vấn đề cần hỗ trợ, vui lòng liên hệ: <a target="_blank" rel="noopener" href="https://t.me/bo_dev">
-        t.me/bo_dev</a>. Trân trọng!</span>
+        t.me/signatureOOO</a>. Trân trọng!</span>
 </div>
